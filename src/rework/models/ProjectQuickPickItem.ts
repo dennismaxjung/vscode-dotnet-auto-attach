483be5fefd6ad9d0c09e2c6ad892769ab9f38e46
Dennis Jung <dennis.max.jung@gmail.com>
2018-06-15 16:45:49 +0200

    Added comments

--- src/rework/models/ProjectQuickPickItem.ts
+++ src/rework/models/ProjectQuickPickItem.ts
@@ -1,13 +1,33 @@
+/*
+ * @file Contains the ProjectQuickPickItem
+ * @Author: Dennis Jung
+ * @Author: Konrad Müller
+ * @Date: 2018-06-15 16:42:53
+ * @Last Modified by: Dennis Jung
+ * @Last Modified time: 2018-06-15 16:43:29
+ */
+
 import { QuickPickItem, Uri } from "vscode";
 
+/**
+ * The ProjectQuickPickItem.
+ *
+ * @export
+ * @class ProjectQuickPickItem
+ * @implements {QuickPickItem}
+ */
 export default class ProjectQuickPickItem implements QuickPickItem {
+	/**
+	 * Creates an instance of ProjectQuickPickItem.
+	 * @param {Uri} uri
+	 * @memberof ProjectQuickPickItem
+	 */
 	public constructor(uri: Uri) {
 		const name_regex = /^.+(\/|\\)(.+).csproj/;
 		let matches = name_regex.exec(uri.fsPath);
 		if (matches && matches.length === 3) {
 			this.label = matches[2];
 		}
-		//this.description = "desc";
 		this.detail = uri.fsPath;
 		this.uri = uri;
 
