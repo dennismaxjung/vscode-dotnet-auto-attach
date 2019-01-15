dfede90f19417331509f818b410eccf95eb99f2d
Dennis Jung <dennis.max.jung@gmail.com>
2019-01-15 12:27:27 +0100

    Added InitialConfigurations & launch.json now be generated if not present

--- src/dotNetAutoAttachDebugConfigurationProvider.ts
+++ src/dotNetAutoAttachDebugConfigurationProvider.ts
@@ -4,7 +4,7 @@
  * @Author: Konrad Müller
  * @Date: 2018-06-15 14:46:07
  * @Last Modified by: Dennis Jung
- * @Last Modified time: 2018-06-15 14:51:54
+ * @Last Modified time: 2019-01-15 12:00:43
  */
 
 import {
@@ -41,6 +41,11 @@ export default class DotNetAutoAttachDebugConfigurationProvider
 		debugConfiguration: IDotNetAutoAttachDebugConfiguration,
 		token?: CancellationToken
 	): ProviderResult<IDotNetAutoAttachDebugConfiguration> {
+		if (!debugConfiguration.type) {
+			// If the config doesn't look functional force VSCode to open a configuration file https://github.com/Microsoft/vscode/issues/54213
+			return null;
+		}
+
 		if (folder) {
 			debugConfiguration.workspace = folder;
 			DotNetAutoAttach.TaskService.StartDotNetWatchTask(debugConfiguration);
