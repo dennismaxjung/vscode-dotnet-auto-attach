87dc91c6135e6a82c83abc7a4f8b12b1fffc1376
Dennis Jung <dennis.max.jung@gmail.com>
2019-02-16 23:50:03 +0100

    Added provideDebugConfigurations, Added MutlipleProjectsEnum, Added comments to IDotNetAutoAttachDebugConfiguration, Added MultipleProjectsFoundInformationMessage

---
+++ src/enums/MultipleProjectsEnum.ts
@@ -0,0 +1,20 @@
+/*
+ * @file Contains the MultipleProjectsEnum.
+ * @Author: Dennis Jung
+ * @Author: Konrad Müller
+ * @Date: 2019-02-02 11:14:20
+ * @Last Modified by: Dennis Jung
+ * @Last Modified time: 2019-02-16 22:23:23
+ */
+
+/**
+ * The MultipleProjectsEnum.
+ *
+ * @export
+ * @enum {string}
+ */
+export enum MultipleProjectsEnum {
+	Yes = "YES",
+	No = "NO",
+	Exit = "EXIT"
+}
